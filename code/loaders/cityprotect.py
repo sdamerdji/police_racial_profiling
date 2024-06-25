b12168b07db9037810a818f7c1c9e4f65322258b
Ava Baker <ava.baker@duke.edu>
2024-06-25 09:37:19 -0700

    improved attempt_nom

--- code/loaders/cityprotect.py
+++ code/loaders/cityprotect.py
@@ -41,6 +41,9 @@ cities_in_scc = ["Alviso",
 				"Willow Glen"
 ]
 
+zipcode_lookup = {95035: 'Milpitas', 95123: 'San Jose', 95020: 'Gilroy', 95014: 'Cupertino', 95051: 'Santa Clara', 95111: 'San Jose', 94087: 'Sunnyvale', 95112: 'San Jose', 95125: 'San Jose', 95122: 'San Jose', 95116: 'San Jose', 95124: 'San Jose', 95136: 'San Jose', 94086: 'Sunnyvale', 95148: 'San Jose', 95008: 'Campbell', 94303: 'Palo Alto', 95050: 'Santa Clara', 95132: 'San Jose', 95120: 'San Jose', 95129: 'San Jose', 94040: 'Mountain View', 95121: 'San Jose', 95126: 'San Jose', 95118: 'San Jose', 95131: 'San Jose', 95070: 'Saratoga', 94043: 'Mountain View', 95134: 'San Jose', 95117: 'San Jose', 95133: 'San Jose', 94306: 'Palo Alto', 94089: 'Sunnyvale', 95054: 'Santa Clara', 94085: 'Sunnyvale', 94024: 'Los Altos', 95135: 'San Jose', 95110: 'San Jose', 94022: 'Los Altos', 94301: 'Palo Alto', 94041: 'Mountain View', 95030: 'Los Gatos', 95130: 'Campbell', 95119: 'San Jose', 95139: 'San Jose', 95046: 'San Martin', 95113: 'San Jose', 94308: 'Santa Clara', 95053: 'Santa Clara', 95002: 'San Jose', 95015: 'Santa Clara', 95031: 'San Jose', 95038: 'Morgan Hill', 95159: 'Santa Clara', 95192: 'San Jose', 95056: 'Santa Clara', 95109: 'Santa Clara', 95150: 'Santa Clara', 94039: 'Mountain View'}
+
+
 def protect_read_csv(f):
 	try:
 		return pd.read_csv(f)
@@ -94,67 +97,51 @@ def cityprotect(target_dir, start_date=datetime(year=2017, month=1, day=1),
 
 	return df
 
-def new_attempt_nom(nom, blocksizedAddr):
-	# before calling this, you'll need to do something like this
-	#         nom = Nominatim(user_agent="policedata", scheme='http')
-	# Then pass in 'nom' as the first argument.
+def get_city(address_dict):
+	for i in ["town", "village", "city"]:
+		if i in address_dict.keys():
+			return address_dict[i]
+	return None
 
-	place = re.sub("BLOCK", "", blocksizedAddr.upper()) + ", SANTA CLARA COUNTY, CA"
-	try:
-		location = nom.geocode(place, addressdetails=True)
-	except:
-		print("Nom: failed")
-		return None
-	if location:
-		address = location.raw['address']                       
-		try:
-			county_idx = address['county']
-		except ValueError:
-			print("Nom: SCC key not found: {}".format(location.raw['display_name']))
-			return None
-		print("Nom: {}".format(location.raw["display_name"]))
-		try:
-			city = address['town']
-		except:
-			print(address.keys)
-		postcode = address['postcode']
-		if city in cities_in_scc:
-			return city, postcode
-		else:
-			print("Nom: {} not a city in Santa Clara County".format(city))
-			return None	
-	else:
-		print("Nom returned None.")
-		return None
-	
 def attempt_nom(nom, blocksizedAddr):
 	# before calling this, you'll need to do something like this
 	#         nom = Nominatim(user_agent="policedata", scheme='http')
 	# Then pass in 'nom' as the first argument.
 
-	place = re.sub("BLOCK", "", blocksizedAddr.upper()) + ", SANTA CLARA COUNTY, CA"
+	place = re.sub(" BLOCK", "", blocksizedAddr.upper()) # take out "BLOCK" if present
+	# specify Santa Clara County if it's not already there
+	if "SANTA CLARA COUNTY" not in place:
+		place = place + ", SANTA CLARA COUNTY"
+	place = place.replace("  ", " ") # remove double spaces
+	place = place.replace("  ", " ") # remove triple spaces
+
+	# locate address details
 	try:
-		location = nom.geocode(place)
+		location = nom.geocode(place, addressdetails=True)
 	except:
-		print("Nom: failed")
+		print("Nom: failed, {}".format(place))
 		return None
+	
+	# find city and postcode
 	if location:
-		display_name = [s.strip() for s in location.raw["display_name"].split(",")]                         
-		try:
-			county_idx = display_name.index("Santa Clara County")
-		except ValueError:
-			print("Nom: SCC key not found: {}".format(display_name))
-			return None
-		print("Nom: {}".format(location.raw["display_name"]))
-		city = display_name[county_idx-1]
-		postcode = display_name[-2]
-		if city in cities_in_scc:
-			return city, postcode
+		address = location.raw['address']                       
+		if "postcode" in address.keys():
+			city = get_city(address) # search address dict for town/city/village
+			if city is None and address["postcode"] in zipcode_lookup.keys():
+				city = zipcode_lookup[int(address["postcode"])] # if no city listed, check if city can be found from zipcode dict
+			if city and city in cities_in_scc:
+				return city, address["postcode"]
+			else:
+				print("Nom: could not locate city or not in Santa Clara County of: {}".format(place))
 		else:
-			print("Nom: {} not a city in Santa Clara County".format(city))
-			return None	
+			print("Couldn't find postcode.")
+			# if there is no house number, add one and check again
+			if not any(chr.isdigit() for chr in place):
+				print("Nom: adding house number")
+				place = "1000 " + place
+				return (attempt_nom(nom, place))
 	else:
-		print("Nom returned None.")
+		print("Nom could not find postcode/city of: {}".format(place))
 		return None
 
         
